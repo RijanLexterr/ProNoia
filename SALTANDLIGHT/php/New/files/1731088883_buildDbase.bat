06f90171fc524f8be952a6704a0a623f14f120a7
RijanLexterr <94050658+RijanLexterr@users.noreply.github.com>
2024-11-09 05:06:08 +0800

    teste

--- SALTANDLIGHT/php/New/files/1731088883_buildDbase.bat
+++
@@ -1,7 +0,0 @@
-echo off
-sqlcmd -E -S LEX -i DatabseCreation.sql
-sqlcmd -E -S LEX -i CreateTables.sql
-sqlcmd -E -S LEX -i SP_User.sql
-sqlcmd -E -S LEX -i SP_ProductType.sql
-sqlcmd -E -S LEX -i SEED_AddAdmin.sql
-set /p delExit=Press the ENTER key to exit...:
